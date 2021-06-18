3ada22c36f5f0aaa089715a8d4b6194d9c6641c8
Ashish Panigrahi <ashish.panigrahi@protonmail.com>
2021-06-18 13:50:19 +0530

    updated `utils.jl`

--- utils.jl
+++ utils.jl
@@ -19,51 +19,17 @@ function hfun_timestamp_now()
     return string(Dates.now()) * "+00:00"
 end
 
-"""
-    {{blogposts}}
-Plug in the list of blog posts contained in the `/posts` folder.
-Souce: <https://github.com/abhishalya/abhishalya.github.io>.
-"""
-function hfun_blogposts()
-    today = Dates.today()
-    curyear = year(today)
-    curmonth = month(today)
-    curday = day(today)
-
+function hfun_recentblogposts()
     list = readdir("blog")
-    filter!(endswith(".md"), list)
-    function sorter(p)
-        ps  = splitext(p)[1]
-        url = "/blog/$ps/"
-        surl = strip(url, '/')
-        pubdate = pagevar(surl, :published)
-        if isnothing(pubdate)
-            return Date(Dates.unix2datetime(stat(surl * ".md").ctime))
-        end
-        return Date(pubdate, dateformat"d U Y")
-    end
-    sort!(list, by=sorter, rev=true)
-
+    filter!(f -> endswith(f, ".md"), list)
+    dates = [stat(joinpath("blog", f)).mtime for f in list]
+    perm = sortperm(dates, rev=true)
+    idxs = perm[1:min(3, length(perm))]
     io = IOBuffer()
-    write(io, """<ul class="blog-posts">""")
-    for (i, post) in enumerate(list)
-        if post == "index.md"
-            continue
-        end
-        ps  = splitext(post)[1]
-        write(io, "<li><span><i>")
-        url = "/blog/$ps/"
-        surl = strip(url, '/')
-        title = pagevar(surl, :title)
-        pubdate = pagevar(surl, :published)
-        description = pagevar(surl, :description)
-        if isnothing(pubdate)
-            date = "$curyear-$curmonth-$curday"
-        else
-            date = Date(pubdate, dateformat"d U Y")
-        end
-        write(io, """$date</i></span><b><a href="$url">$title</a></b>""")
-        write(io, """<li><i class="description">$description</i></li>""")
+    write(io, "<ul>")
+    for (k, i) in enumerate(idxs)
+        fi = "/blog/" * splitext(list[i])[1] * "/"
+        write(io, """<li><a href="$fi">Post $k</a></li>\n""")
     end
     write(io, "</ul>")
     return String(take!(io))
